1d970614782d75f2ba64acb70bc822ce29508b03
Scott Vandehey <fake-scott@spaceninja.com>
2018-11-11 10:32:20 -0800

    tweet styles wip

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import Icon from "./Icon";
 import moment from "moment-twitter";
 
 class Tweet extends React.Component {
@@ -7,12 +8,33 @@ class Tweet extends React.Component {
     const { user, thread, ...tweet } = this.props.details;
     const avatarURL = "https://pbs.twimg.com/profile_images";
     const timestamp = moment(tweet.created_at);
-    const now = moment("2014-01-17T03:21:00.000Z");
+    const now = moment("2014-01-17T03:21:00.000Z"); // hack to make old tweets seem current
     const diff = now.diff(timestamp);
     const timeAgo = moment(moment() - diff).twitterShort();
     const date = timestamp.format("h:mm A - D MMM YYYY");
+    let classes = "tweet";
+    if (tweet.retweeted_by) classes += " tweet--retweet";
+    if (thread) {
+      classes += " tweet--thread";
+      if (thread.start) classes += " tweet--thread-start";
+      if (thread.end) classes += " tweet--thread-end";
+      if (thread.collapsed) classes += " is-collapsed";
+    }
+    if (thread && thread.collapsed) {
+      return (
+        <article className={classes}>
+          <div className="tweet__collapsed-text">
+            <Button
+              component="tweet__show-more"
+              className="btn--link"
+              text="1 more reply"
+            />
+          </div>
+        </article>
+      );
+    }
     return (
-      <article className="tweet">
+      <article className={classes}>
         {/* eslint-disable jsx-a11y/anchor-is-valid */}
         <div className="tweet__avatar">
           <a href="#">
@@ -28,10 +50,22 @@ class Tweet extends React.Component {
             />
           </a>
         </div>
+        {tweet.retweeted_by && (
+          <div className="tweet__retweet-info">
+            <Icon className="tweet__retweet-icon" type="icon-retweet" />
+            <span className="tweet__retweet-by">
+              Retweeted by {tweet.retweeted_by.name}
+            </span>
+          </div>
+        )}
         <div className="tweet__header">
           <span className="tweet__user">
-            <strong className="tweet__user-name">{user.name}</strong>{" "}
-            <span className="tweet__user-screen-name">@{user.screen_name}</span>{" "}
+            <a className="tweet__user-link" href="#">
+              <strong className="tweet__user-name">{user.name}</strong>{" "}
+              <span className="tweet__user-screen-name">
+                @{user.screen_name}
+              </span>
+            </a>
           </span>
           <time
             className="tweet__timestamp"
