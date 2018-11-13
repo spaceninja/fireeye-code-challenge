e86b58d6ce90c24519c61a6389e5c6c0bf426d81
Scott Vandehey <fake-scott@spaceninja.com>
2018-11-13 10:05:45 -0800

    tweet composer styles

--- src/components/TweetComposer.js
+++ src/components/TweetComposer.js
@@ -5,6 +5,7 @@ const maxLength = 140;
 
 class Composer extends React.Component {
   state = {
+    expanded: this.props.expanded,
     tweet: this.props.tweetString,
     remaining: this.props.tweetString
       ? maxLength - this.props.tweetString.length
@@ -18,6 +19,10 @@ class Composer extends React.Component {
     });
   };
 
+  handleFocus = () => {
+    this.setState({ expanded: true });
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     this.setState({ tweet: "" });
@@ -27,23 +32,34 @@ class Composer extends React.Component {
   render() {
     return (
       <form
-        className={`new-tweet ${this.props.className}`}
+        className={`new-tweet ${this.props.className} ${
+          this.state.expanded ? "is-expanded" : ""
+        }`}
         onSubmit={this.handleSubmit}
+        onFocus={this.handleFocus}
       >
-        <label htmlFor="new-tweet" className="new-tweet__label sr-only">
-          Compose new Tweet
-        </label>
-        <p>{this.props.expanded ? "EXPANDED" : "COLLAPSED"}</p>
+        {this.state.expanded && (
+          <label htmlFor="new-tweet" className="new-tweet__label">
+            What's Happening?
+          </label>
+        )}
         <textarea
           className="new-tweet__input"
           id="new-tweet"
           maxLength={maxLength}
           onChange={this.handleChange}
           placeholder="Compose new Tweet"
+          rows={this.state.expanded ? "3" : "1"}
           value={this.state.tweet}
         />
-        <p>{this.state.remaining} characters remaining</p>
-        <Button component="new-tweet__submit" text="Submit" type="submit" />
+        {this.state.expanded && (
+          <div className="new-tweet__actions">
+            <p className="new-tweet__remaining">
+              {this.state.remaining} characters remaining
+            </p>
+            <Button component="new-tweet__submit" text="Tweet" type="submit" />
+          </div>
+        )}
       </form>
     );
   }
